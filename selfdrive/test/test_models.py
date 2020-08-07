ad4e7d3a043ce0eb4747792d45ebf8edfad970da
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-08-07 12:47:18 -0700

    check controls allowed

--- selfdrive/test/test_models.py
+++ selfdrive/test/test_models.py
@@ -166,6 +166,7 @@ class TestCarModel(unittest.TestCase):
       # check that openpilot and panda safety agree on the car's state
       checks['gasPressed'] += CS.gasPressed != safety.get_gas_pressed_prev()
       checks['brakePressed'] += CS.brakePressed != safety.get_brake_pressed_prev()
+      checks['controlsAllowed'] += CS.cruiseState.enabled != safety.get_vehicle_moving()
 
     # TODO: test should fail for any differences
     failed_checks = {k: v for k, v in checks.items() if v > 5}
